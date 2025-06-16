76a344d7971d7bd41004be5bbe1e6dfdb9076a62
Easir Arafat <riyadhbd2@Easirs-MacBook-Air.local>
2025-06-16 11:36:30 +0200

    frontend basic setup added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { Toaster } from "react-hot-toast";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Favorite from "./pages/Favorite";
+import Home from "./pages/Home";
+import MovieDetails from "./pages/MovieDetails";
+import Movies from "./pages/Movies";
+import MyBookings from "./pages/MyBookings";
+import SeatLayout from "./pages/SeatLayout";
 
 const App = () => {
+  const isAdminRoute = useLocation().pathname.startsWith("/admin");
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Toaster />
+      {!isAdminRoute && <Navbar />}
 
-export default App
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
+        <Route path="/movies/:id/:date" element={<SeatLayout />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/favorite" element={<Favorite />} />
+      </Routes>
+      {!isAdminRoute && <Footer />}
+    </>
+  );
+};
+
+export default App;
